3875b4c3d43b955536d3e6c0d06bc8372e94612e
sbanik <contactsouravbanik@gmail.com>
2026-09-14 13:29:03 +0530

    Update documax to 0.1.3

--- Formula/documax.rb
+++ Formula/documax.rb
@@ -1,8 +1,8 @@
 class Documax < Formula
   desc "Package and restore directory trees as portable documents"
   homepage "https://github.com/sbanik/documax"
-  url "https://github.com/sbanik/documax/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "a0c3ec26a2828dcbaed37e801d0098abb17c1603d5fffc9e38151811bc589057"
+  url "https://github.com/sbanik/documax/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "ce7291f1bb8b9de450d62753942fece58a0d534cf46fd0e50bb96dc7c156dc33"
   license "MIT"
 
   depends_on "go" => :build
